4165881e8fd2be21dde438574e8607a0d1358f02
grandsmarquis <jdourlens@gmail.com>
2024-08-24 15:53:43 +0900

    fix

--- frontend/src/components/disputeButton.tsx
+++ frontend/src/components/disputeButton.tsx
@@ -1,4 +1,4 @@
-import styles from '../styles/Home.module.css';
+import styles from '../styles/body.module.css';
 import { useAccount } from 'wagmi';
 import { useState } from 'react';
 import { writeContract, waitForTransactionReceipt } from '@wagmi/core';
