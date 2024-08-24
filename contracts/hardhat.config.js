de2640773e5f2b679fbe0a278b9f85c2d9d4ba31
grandsmarquis <jdourlens@gmail.com>
2024-08-24 10:26:25 +0900

    hardhat conf

--- contracts/hardhat.config.js
+++ contracts/hardhat.config.js
@@ -1,6 +1,19 @@
-require("@nomicfoundation/hardhat-toolbox");
+equire("@nomicfoundation/hardhat-toolbox");
+
+require("dotenv").config();
+
+let privateKey = process.env.PRIVATE_KEY;
+
+const defaultNetwork = "sepolia";
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
+  defaultNetwork,
+  networks: {
+    sepolia: {
+      url: "https://rpc.sepolia.org",
+      accounts: [privateKey]
+    }
+  }
 };
