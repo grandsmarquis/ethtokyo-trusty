d1d759a7e0ce27acf48029724bebad4e9e4a401e
Kyoya67 <kyoya24@iamas.ac.jp>
2024-08-24 22:05:53 +0900

    delete warning text

--- frontend/src/components/downvoteButton.jsx
+++ frontend/src/components/downvoteButton.jsx
@@ -11,13 +11,12 @@ import { faThumbsDown } from '@fortawesome/free-solid-svg-icons';
 const Body = (props) => {
 
     const [isSendingTx, setIsSendingTx] = useState(false);
-    const [showTooltip, setShowTooltip] = useState(false); // Tooltip用の状態
 
     async function downvote(id) {
         if (props.alreadyVoted) {
             return;
         }
-        console.log(id);
+        console.log(id)
         try {
             let tx = await writeContract(config, {
                 abi,
@@ -28,7 +27,7 @@ const Body = (props) => {
             setIsSendingTx(true);
             const transactionReceipt = await waitForTransactionReceipt(config, {
                 hash: tx,
-            });
+            })
             props.onSuccess();
         } catch (error) {
             setIsSendingTx(false);
@@ -43,21 +42,13 @@ const Body = (props) => {
             {isSendingTx ? (
                 <div className={styles.loader}></div>
             ) : (
-                <div
-                    className={styles.voteSection}
-                    onMouseEnter={() => props.alreadyVoted && setShowTooltip(true)}
-                    onMouseLeave={() => setShowTooltip(false)}
-                >
+                <div className={styles.voteSection}>
                     <button
                         className={`${styles.iconButton} ${props.alreadyVoted ? styles.disabledButton : ''}`}
                         onClick={() => downvote(Number(props.id))}
-                        disabled={props.alreadyVoted}
                     >
                         <FontAwesomeIcon icon={faThumbsDown}></FontAwesomeIcon>
                     </button>
-                    {showTooltip && props.alreadyVoted && (
-                        <div className={styles.tooltipText}>You have already voted</div>
-                    )}
                 </div>
             )}
         </span>
