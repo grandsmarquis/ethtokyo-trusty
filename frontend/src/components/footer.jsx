518bcde34021c76a96cba07a82a86ac977d29773
Kyoya67 <kyoya24@iamas.ac.jp>
2024-08-24 22:09:59 +0900

    footer modified

--- frontend/src/components/footer.jsx
+++ frontend/src/components/footer.jsx
@@ -7,18 +7,7 @@ const Footer = () => {
         <div className={styles.footerColumn}>
           <h3>Learn More</h3>
           <ul>
-            <li><a href="#">Learn more</a></li>
-            <li><a href="#">Github</a></li>
-            <li><a href="#">Documentation</a></li>
-            <li><a href="#">White Paper</a></li>
-          </ul>
-        </div>
-        <div className={styles.footerColumn}>
-          <h3>Join Us</h3>
-          <ul>
-            <li><a href="#">Discord</a></li>
-            <li><a href="#">Twitter</a></li>
-            <li><a href="#">Medium</a></li>
+            <li><a href="https://github.com/grandsmarquis/ethtokyo-trusty" target="blank">Github</a></li>
           </ul>
         </div>
       </div>
